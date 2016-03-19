e8b37f22bfda570e94e0e56bbd3d7e59c124d80b
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-03-19 23:18:49 +0100

    Changed bias input to -1, working self-training OR example

--- src/no/uials/birger/ann/ArtificialNeuralNetworkApp.java
+++ src/no/uials/birger/ann/ArtificialNeuralNetworkApp.java
@@ -6,6 +6,7 @@ import java.util.function.DoubleFunction;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.xy.XYSplineRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -31,27 +32,83 @@ public class ArtificialNeuralNetworkApp {
 
 	public ArtificialNeuralNetworkApp() {
 
-		DoubleFunction<Double> f = (double x) -> 1 / (1 + Math.exp(-x));
-		DoubleFunction<Double> fD = (double x) -> f.apply(x) * (1 - f.apply(x));
+		trainOrNetwork();
 
-		double[] h1 = { .15, .2, .35 };
-		double[] h2 = { .25, .3, .35 };
+	}
 
-		double[] o1 = { .4, .45, .6 };
-		double[] o2 = { .5, .55, .6 };
+	public void trainOrNetwork() {
 
-		double[][] l1 = { h1, h2 };
-		double[][] l2 = { o1, o2 };
+		XYSeries w0 = new XYSeries("w0");
+		XYSeries w1 = new XYSeries("w1");
+		XYSeries w2 = new XYSeries("w2");
 
-		double[][][] w = { l1, l2 };
+		DoubleFunction<Double> tanh = (double x) -> Math.tanh(x);
+		DoubleFunction<Double> tanhDiff = (double x) -> 1 - Math.pow(tanh.apply(x), 2);
 
-		Network network = new Network(f, fD, w);
+		Network network = Network.getRandom(tanh, tanhDiff, 2, 1);
 
-		double[] i = { .05, .1 };
+		int workouts = 0;
+		int maxWorkouts = 50;
+		int attempts = 1000000;
 
-		double[] ideal = {0.01, 0.99};
+		double[] w = network.getWeights()[0][0];
+		w0.add(workouts, w[0]);
+		w1.add(workouts, w[1]);
+		w2.add(workouts, w[2]);
 
-		network.train(i, ideal, 0.5);
+		double[] x = new double[2];
+		double[] ideal = new double[1];
+		double result;
+
+		boolean pass = false;
+		while (!pass && workouts <= maxWorkouts) {
+
+			pass = true;
+			for (int i = 0; i < attempts && pass; i++) {
+
+				x[0] = Math.random() >= 0.5 ? 1 : -1;
+				x[1] = Math.random() >= 0.5 ? 1 : -1;
+				result = (network.recall(x)[0] > 0 ? 1 : -1);
+				ideal[0] = x[0] + x[1] >= 0 ? 1 : -1;
+
+				if (result != ideal[0]) {
+
+					pass = false;
+					network.train(x, ideal, 0.25);
+
+					workouts++;
+
+					w = network.getWeights()[0][0];
+					w0.add(workouts, w[0]);
+					w1.add(workouts, w[1]);
+					w2.add(workouts, w[2]);
+
+				}
+
+			}
+
+		}
+
+		w = network.getWeights()[0][0];
+		w0.add(workouts + 1, w[0]);
+		w1.add(workouts + 1, w[1]);
+		w2.add(workouts + 1, w[2]);
+
+		if (pass)
+			System.out.println("It took " + workouts + " workouts to handle " + attempts + " attempts");
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(w0);
+		dataset.addSeries(w1);
+		dataset.addSeries(w2);
+
+		JFreeChart chart = ChartFactory.createXYLineChart(null, null, null, dataset, PlotOrientation.VERTICAL, true,
+				true, false);
+		ChartFrame frame = new ChartFrame("pass: " + pass, chart);
+		frame.pack();
+		frame.setVisible(true);
+
+		show2DNetwork(network);
 
 	}
 
@@ -62,8 +119,8 @@ public class ArtificialNeuralNetworkApp {
 		double[] input = new double[2];
 		double output;
 
-		for (double y = 0; y <= 1; y += 0.025) {
-			for (double x = 0; x <= 1; x += 0.025) {
+		for (double y = -1; y <= 1; y += 0.025) {
+			for (double x = -1; x <= 1; x += 0.025) {
 
 				input[0] = x;
 				input[1] = y;

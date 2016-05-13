25c2bff29788d0de69bcb89ca7173126bb255109
Birger Skogeng Pedersen <birgersp@ntnu.no>
2016-05-13 14:57:50 +0200

    Made automatic training algorithm

--- src/no/uials/birger/ann/ArtificialNeuralNetworkApp.java
+++ src/no/uials/birger/ann/ArtificialNeuralNetworkApp.java
@@ -22,7 +22,7 @@ public class ArtificialNeuralNetworkApp {
         DoubleFunction<Double> f = (double x) -> Math.tanh(x);
         DoubleFunction<Double> fD = (double x) -> 1 - Math.pow(f.apply(x), 2);
 
-        final Network network = Network.getRandom(f, fD, 2, 2, 1);
+        final Network network = Network.getRandom(f, fD, 2, 20, 1);
 
         final XYSeries trainingSeries = new XYSeries("training");
         final XYSeries testingSeries = new XYSeries("testing");
@@ -47,7 +47,7 @@ public class ArtificialNeuralNetworkApp {
         int nTest = 500;
         final double[] trainingRate = {0.01};
 
-        double range = 1;
+        double range = 4;
 
         final DoubleFunction<Double> targetFunction = (double x) -> Math.sin((x - 0.5) * Math.PI);
 
@@ -93,84 +93,6 @@ public class ArtificialNeuralNetworkApp {
                         break;
                     case KeyEvent.VK_ESCAPE:
                         System.exit(0);
-                    case KeyEvent.VK_SPACE:
-                        print3DDoubleArray(network.getWeights());
-                        neuralOutput.clear();
-                        for (double x = -range; x <= range; x += 0.05) {
-                            for (double y = -range; y <= range; y += 0.05) {
-
-                                double[] input2 = {x, y};
-                                double[] output2 = network.recallAndActivate(input2);
-
-                                if (output2[0] > 0.0) {
-                                    neuralOutput.add(x, y);
-                                    break;
-                                }
-
-                            }
-
-                        }
-                        break;
-                    case KeyEvent.VK_ENTER:
-                        int trainErrors = 0;
-                        int testErrors = 0;
-                        double[] input = null;
-                        double[] output = null;
-                        double[] target = new double[1];
-                        for (int i = 0; i < nTrain; i++) {
-
-                            input = trainingSet[i];
-                            output = network.recallAndActivate(input);
-
-                            if (input[1] < targetFunction.apply(input[0])) { // 0
-
-                                target[0] = -1;
-                                if (output[0] > 0) {
-                                    trainErrors++;
-                                }
-
-                            } else { // 1
-
-                                target[0] = 1;
-                                if (output[0] <= 0) {
-                                    trainErrors++;
-                                }
-
-                            }
-
-                            network.train(input, target, trainingRate[0]);
-
-                        }
-
-                        for (int i = 0; i < nTest; i++) {
-
-                            input = testingSet[i];
-                            output = network.recallAndActivate(input);
-
-                            if (input[1] < targetFunction.apply(input[0])) { // 0
-
-                                target[0] = -1;
-                                if (output[0] > 0) {
-                                    testErrors++;
-                                }
-
-                            } else { // 1
-
-                                target[0] = 1;
-                                if (output[0] <= 0) {
-                                    testErrors++;
-                                }
-
-                            }
-
-                        }
-
-                        double trainingError = (double) trainErrors / (double) nTrain;
-                        double testingError = (double) testErrors / (double) nTest;
-                        int epochs = trainingSeries.getItemCount();
-                        trainingSeries.add(epochs, trainingError);
-                        testingSeries.add(epochs, testingError);
-                        break;
                     default:
                         break;
                 }
@@ -188,24 +110,116 @@ public class ArtificialNeuralNetworkApp {
 //        dataset.addSeries(rejected);
 
         XYSeries sine = new XYSeries("Sine");
-        double y;
         for (double x = -range; x <= range; x += 0.1) {
-            y = targetFunction.apply(x);
-            sine.add(x, y);
+            sine.add(x, targetFunction.apply(x));
         }
         dataset.addSeries(sine);
 
         JFreeChart chart = ChartFactory.createScatterPlot(null, null, null,
                 dataset);
         chart.getXYPlot().setRenderer(new XYSplineRenderer());
-        chart.setAntiAlias(true);
         chart.getXYPlot().getRenderer().setSeriesPaint(0, Color.BLUE);
         chart.getXYPlot().getRenderer().setSeriesPaint(1, Color.RED);
         chart.getXYPlot().getRenderer().setSeriesPaint(2, Color.BLACK);
+        chart.getXYPlot().getRangeAxis().setAutoRange(false);
         ChartFrame frame = new ChartFrame("2D Network", chart);
+        
+        chart.setAntiAlias(true);
+        
         frame.addKeyListener(keyListener);
         frame.pack();
         frame.setVisible(true);
+
+        int epochs = 0;
+        while (true) {
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+
+//            print3DDoubleArray(network.getWeights());
+            neuralOutput.clear();
+
+            for (double x = -range; x <= range; x += 0.1) {
+                for (double y = -range; y <= range; y += 0.1) {
+
+                    double[] input2 = {x, y};
+                    double[] output2 = network.recallAndActivate(input2);
+
+                    if (output2[0] > 0.0) {
+                        neuralOutput.add(x, y);
+                        break;
+                    }
+
+                }
+
+            }
+
+            int trainErrors = 0;
+            int testErrors = 0;
+            double[] input = null;
+            double[] output = null;
+            double[] target = new double[1];
+            for (int i = 0; i < nTrain; i++) {
+
+                input = trainingSet[i];
+                output = network.recallAndActivate(input);
+
+                if (input[1] < targetFunction.apply(input[0])) { // 0
+
+                    target[0] = -1;
+                    if (output[0] > 0) {
+                        trainErrors++;
+                    }
+
+                } else { // 1
+
+                    target[0] = 1;
+                    if (output[0] <= 0) {
+                        trainErrors++;
+                    }
+
+                }
+
+                network.train(input, target, trainingRate[0]);
+
+            }
+
+            for (int i = 0; i < nTest; i++) {
+
+                input = testingSet[i];
+                output = network.recallAndActivate(input);
+
+                if (input[1] < targetFunction.apply(input[0])) { // 0
+
+                    target[0] = -1;
+                    if (output[0] > 0) {
+                        testErrors++;
+                    }
+
+                } else { // 1
+
+                    target[0] = 1;
+                    if (output[0] <= 0) {
+                        testErrors++;
+                    }
+
+                }
+
+            }
+
+            if (epochs == 0 || epochs % 10 == 0) {
+                double trainingError = (double) trainErrors / (double) nTrain;
+                double testingError = (double) testErrors / (double) nTest;
+                trainingSeries.add(epochs, trainingError);
+                testingSeries.add(epochs, testingError);
+            }
+            
+            epochs++;
+        }
 
     }
 
